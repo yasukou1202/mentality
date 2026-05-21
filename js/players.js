bb06efe209cce94d044eb8f343950368cd8b8906
yasukou1202 <yasukou1202@gmail.com>
2026-05-22 01:04:20 +0900

    fix history semicolon bug, fix MIN to per-game average

--- js/players.js
+++ js/players.js
@@ -255,7 +255,7 @@ async function openPlayerDetail(name, team) {
     const pct = (v) => v !== undefined ? (v * 100).toFixed(1) + '%' : '-';
 
     const statsItems = [
-      {k:'GP',  v:cur.games},           {k:'MIN', v:cur.minutesPg?.toFixed(1)},
+      {k:'GP',  v:cur.games},           {k:'MIN', v:cur.minutes&&cur.games?(cur.minutes/cur.games).toFixed(1):cur.minutesPg?.toFixed(1)},
       {k:'PTS', v:fmt(cur.points)},     {k:'REB', v:fmt(cur.totalRb)},
       {k:'AST', v:fmt(cur.assists)},    {k:'STL', v:fmt(cur.steals)},
       {k:'BLK', v:fmt(cur.blocks)},     {k:'TO',  v:fmt(cur.turnovers)},
@@ -290,8 +290,7 @@ async function openPlayerDetail(name, team) {
             <div style="font-size:.7rem;font-weight:700;color:var(--tx);">${s.v}</div>
           </div>`).join('')}
         </div>
-      </div>\`;
-      </tr>`;
+      </div>`;
     }).join('');
 
     const awardsData = AWARDS_MAP[name] || [];
